e325887d1ee11eb779bc07539a658cd4e61b3a5e
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-05 22:56:51 +0200

    <feature> function createGrid rows x cols;

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const mainGrid = document.getElementById(`mainGrid`); 
+
+// Make the above const responsive to selectors or player inputs. 
+
+function createGrid (rows, cols) {
+
+ mainGrid.style.setProperty(--grid-rows, rows)
+ mainGrid.style.setProperty(--grid-cols, cols)
+  for (let i=0; i<(rows * cols); i++) {
+    let cell =  document.createElement("div"); 
+    mainGrid.appendChild(cell).className = "grid-item";
+  };
+};
