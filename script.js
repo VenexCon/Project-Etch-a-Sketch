e41dc497fb0d4a949da614d2ca5a89220e14ce6e
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-08 22:42:53 +0100

    <update> redfined makerows() eventlistener uses DOM manip

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-const container = document.getElementById("container");
 
+const gridSizeSelector50 = document.getElementById(`grid-size-selector50`)
+
+
+
+
+
+
+
+// uses a loop to create the required divs
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    //cell.innerText = (c + 1);
+    cell.classes = ['grid-item'];
+    cell.style.setProperty('background-color', 'white');
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
+makeRows(20, 20)
+
+
+// test function for mouseover events 
+function changeGridColor (e) {
+  e.style.backgroundColor="red"; 
+}; 
+
+// This block targets the whole DOM and identifies the dvis with the grid-item class
+document.addEventListener('mouseover',function(e){
+  console.log(e.target.classes);
+  if(e.target && e.target.classes.includes('grid-item')){
+        changeGridColor(e.target);
+   }
+});
+
+
+gridSizeSelector50.addEventListener(`click`, () => {
+  makeRows(50, 50);
+})
