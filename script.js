2c862fbe2ac74c7d8f3ffedcc89b11dc5cd29bad
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-08 20:21:32 +0100

    <update> Included makeRows function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+const container = document.getElementById("container");
 
-const mainGrid = document.getElementById(`mainGrid`); 
-
-// Make the above const responsive to selectors or player inputs. 
-
-function createGrid (rows, cols) {
-
- mainGrid.style.setProperty(--grid-rows, rows)
- mainGrid.style.setProperty(--grid-cols, cols)
-  for (let i=0; i<(rows * cols); i++) {
-    let cell =  document.createElement("div"); 
-    mainGrid.appendChild(cell).className = "grid-item";
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    //cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
   };
 };
+
+makeRows(16, 16);
