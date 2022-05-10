a89f37660ba042d6a65e23f52eae2d78f4863cd7
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-10 16:54:01 +0100

    <fix> changeGridCol now accepts input-color value.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const container = document.getElementById(`container`);
+
+
 
 function createGrid(rows, cols) {
  const container = document.getElementById(`container`);
@@ -5,8 +8,6 @@ function createGrid(rows, cols) {
   for (c = 0; c < rowNum; c++) {
     let cell = document.createElement("div");
     cell.classes = ['grid-item'];
-    cell.addEventListener(`click`, () => {
-      console.log("click");});
     container.appendChild(cell).className = "grid-item";
   };
 };
@@ -18,20 +19,11 @@ const gridItems = document.querySelectorAll(`.grid-item`);
 let gridSizeSelector = document.getElementById('grid-size-selector');
 
 
-
-
-
-/* uses a loop to create the required divs
-function createGrid(rows, cols) {
- const rowNum = (rows * cols); 
-  for (c = 0; c < rowNum; c++) {
-    let cell = document.createElement("div");
-    cell.classes = ['grid-item'];
-    //cell.addEventListener(`click`, () => {
-      //console.log("click");});
-    container.appendChild(cell).className = "grid-item";
-  };
-}; */
+container.addEventListener(`mouseover`, function (e){
+  if(e.target && e.target.classList.contains('grid-item') &&  (drawing == true)){
+    changeGridColor(e.target);
+  } else return ""; 
+})
 
 
 // Drawing declarations 
@@ -46,26 +38,18 @@ let drawing = false;
 
     
 function changeGridColor (e) {
-   if(e.target && e.target.classList.contains('grid-item') && (drawing == true)){
-    e.style.backgroundColor="#ff0000"
-   } else return ""; 
+  e.style.backgroundColor= colorSelector.value; 
 }; 
 
-// This block targets the whole DOM and identifies the divs with the grid-item class
-/* document.addEventListener('mouseover',function(e){
-  //console.log(e.target.classes);
-  if(e.target && e.target.classList.contains('grid-item') && (drawing == true)){
-    changeGridColor(e.target);
-   } else return ;
-}); */
 
-
-
-// Color Selection Function
 let colorSelector = document.querySelector(`input[name="color-selector"]`)
     colorSelector.addEventListener(`input`, function (){ 
     let selectedColor = colorSelector.value;
     console.log(selectedColor);
 });
+
+
+
+
 
 
